1a025581666d52cea117e47fc58a8ae371e82d9f
Manuel Kleinert <info@manuelkleinert.ch>
2024-02-07 16:11:06 +0100

    Update AssetCleanupCommand.php

--- src/Command/AssetCleanupCommand.php
+++ src/Command/AssetCleanupCommand.php
@@ -4,43 +4,34 @@ declare(strict_types=1);
 namespace CleanupBundle\Command;
 
 use Pimcore\Console\AbstractCommand;
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 /**
  * @internal
  */
-#[AsCommand(
-    name: 'cleanup:asset',
-    description: 'Cleanup assets'
-)]
-
 class AssetCleanCleanupCommand extends AbstractCommand
 {
-    public function __construct(private EventDispatcherInterface $eventDispatcher)
-    {
-        parent::__construct();
-    }
-
     protected function configure(): void
     {
-        $this
-            ->addOption(
-                'temp',
-                't',
-                InputOption::VALUE_NONE,
-                'create tmp folder'
-            )
-            ->addOption(
-                'all',
-                'a',
-                InputOption::VALUE_NONE,
-                'Clear all'
-            )
-        ;
+        $this->setName('cleanup:asset');
+        $this->setDescription('Cleanup assets');
+        
+        // $this
+        //     ->addOption(
+        //         'temp',
+        //         't',
+        //         InputOption::VALUE_NONE,
+        //         'create tmp folder'
+        //     )
+        //     ->addOption(
+        //         'all',
+        //         'a',
+        //         InputOption::VALUE_NONE,
+        //         'Clear all'
+        //     )
+        // ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
