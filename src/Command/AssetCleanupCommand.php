66a19e350751849ebf0c556ca8ff263a6e102506
Manuel Kleinert <manuel.kleinert@gmail.com>
2024-02-29 12:48:58 +0100

    update controller

--- src/Command/AssetCleanupCommand.php
+++ src/Command/AssetCleanupCommand.php
@@ -7,10 +7,10 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AssetCleanCleanupCommand extends Command
+class AssetCleanupCommand extends Command
 {
-    protected static $defaultName = 'cleanup:asset';
-    protected static $defaultDescription = 'Cleanup assets';
+    protected static $defaultName = 'cleanup:assets';
+    protected static $defaultDescription = 'cleanup assets';
 
     public function __construct()
     {
@@ -19,7 +19,7 @@ class AssetCleanCleanupCommand extends Command
 
     protected function configure(): void
     {
-        $this->setHidden(false);
+        $this->setHidden(true);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
