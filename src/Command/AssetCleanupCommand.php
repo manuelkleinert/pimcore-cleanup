cb4eb49f73f24e7a7d66d36444d066f3e4bf3c0c
Manuel Kleinert <manuel.kleinert@gmail.com>
2024-02-29 10:21:26 +0100

    update

--- src/Command/AssetCleanupCommand.php
+++ src/Command/AssetCleanupCommand.php
@@ -4,18 +4,37 @@ declare(strict_types=1);
 namespace CleanupBundle\Command;
 
 use Pimcore\Console\AbstractCommand;
+use Pimcore\Event\SystemEvents;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
-/**
- * @internal
- */
+#[AsCommand(
+    name: 'cleanup:asset',
+    description: 'Cleanup assets'
+)]
 class AssetCleanCleanupCommand extends AbstractCommand
 {
+    public function __construct(private EventDispatcherInterface $eventDispatcher)
+    {
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
-        $this->setName('cleanup:asset');
-        $this->setDescription('Cleanup assets');
+        $this
+            ->setHidden(false)
+            ->addOption(
+                'tags',
+                't',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'Only specific tags (csv list of tags)'
+            )
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
