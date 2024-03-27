0a610b8221c0fda94b2d3a32420aedd9aad2bb59
Manuel Kleinert <info@manuelkleinert.ch>
2024-03-27 09:24:26 +0100

    bugfix

--- src/Command/AssetCleanupCommand.php
+++ src/Command/AssetCleanupCommand.php
@@ -3,27 +3,38 @@ declare(strict_types=1);
 
 namespace CleanupBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Pimcore\Console\AbstractCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AssetCleanupCommand extends Command
+#[AsCommand(
+    name: 'cleanup:asset',
+    description: 'Awesome command'
+)]
+class AssetCleanCleanupCommand extends AbstractCommand
 {
-    protected static $defaultName = 'cleanup:assets';
-    protected static $defaultDescription = 'cleanup assets';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this->setHidden(true);
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        return 0;
+        // dump
+        $this->dump("Isn't that awesome?");
+
+        // add newlines through flags
+        $this->dump("Dump #2");
+
+        // only dump in verbose mode
+        $this->dumpVerbose("Dump verbose");
+        
+        // Output as white text on red background.
+        $this->writeError('oh noes!');
+
+        // Output as green text.
+        $this->writeInfo('info');
+
+        // Output as blue text.
+        $this->writeComment('comment');
+
+        // Output as yellow text.
+        $this->writeQuestion('question');
     }
 }
